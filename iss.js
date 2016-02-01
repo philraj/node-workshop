0b5947532068657da732a94a2872db0c1149c29f
Phil Rajchgot <tophil@outlook.com>
2016-02-01 19:41:15 +0000

    Created user prompt for location. This is used to make an http request to google's API to get the user's longitude and lattitude, which is then passed to a function which makes another request, this time to an API that returns the coords of the ISS. These values are then plugged into the haversine formula to get the distance between the user and the ISS.

--- iss.js
+++ iss.js
@@ -1,11 +1,73 @@
 var request = require("request");
+var prompt = require("prompt");
 
-request("http://api.open-notify.org/iss-now.json", function (err, response, body) {
-    if (!err && response.statusCode === 200) {
-        var data = JSON.parse(body);
-        var lat = Number( data.iss_position.latitude.toFixed(2) );
-        var long = Number( data.iss_position.longitude.toFixed(2) );
-        
-        console.log("The position of the ISS is...\nLatitude: " + lat + "\nLongitude: " + long);    
-    }
+Number.prototype.toRadians = function() {
+    return this * Math.PI / 180;
+}
+
+//computes 'as the crow flies' distance between two points on a sphere
+function haversine (lat1, lat2, lon1, lon2) {
+    var R = 6371000; // metres
+    var φ1 = lat1.toRadians();
+    var φ2 = lat2.toRadians();
+    var Δφ = (lat2-lat1).toRadians();
+    var Δλ = (lon2-lon1).toRadians();
+    
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    
+    return R * c;
+}
+
+//rounds floating point numbers to 2 decimal places
+function truncate (num) {
+    return Number(num.toFixed(2));
+}
+
+
+
+prompt.start();
+
+prompt.get( ['location'], function(err, result) {
+    if (err) return console.log("Error! " + err);
+    
+    //make an http request using user input result.location
+    request("https://maps.googleapis.com/maps/api/geocode/json?address=" + result.location,
+        function (err, response, body) {
+            //if there is no error and the http status code is A-OK...
+            if (!err && response.statusCode === 200) {
+                var data = JSON.parse(body)
+                var userLat = data.results[0].geometry.location.lat;
+                var userLong = data.results[0].geometry.location.lng;
+                
+                console.log("User latitude: " + truncate(userLat));
+                console.log("User longitude: " + truncate(userLong));
+                
+                getDistanceFromISS(userLat, userLong);
+            }
+            else {
+                console.log("There was a problem. \nError: " + err + "\nStatus: " + response.statusCode);
+            }
+        }
+    );    
 });
+
+function getDistanceFromISS (userLat, userLong) {
+    request("http://api.open-notify.org/iss-now.json", function(err, response, body) {
+        if (!err && response.statusCode === 200) {
+            var data = JSON.parse(body);
+            var issLat = data.iss_position.latitude;
+            var issLong = data.iss_position.longitude;
+            
+            console.log("The position of the ISS is...\nLatitude: " + truncate(issLat) + "\nLongitude: " + truncate(issLong)); 
+            
+            var distanceFromISS = haversine(userLat, issLat, userLong, issLong);
+            console.log("Your distance from the ISS is " + truncate(distanceFromISS/1000) + " kilometers.");
+        }
+        else {
+            console.log("There was a problem. \nError: " + err + "\nStatus: " + response.statusCode);
+        }
+    });
+}
