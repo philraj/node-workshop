f7e63b5bc287ea34b3d6f369dd8edfba3d10d6db
Phil Rajchgot <tophil@outlook.com>
2016-02-01 21:33:01 +0000

    Added limit to number of times user can guess, and recursive prompt call so that user can guess multiple times.

--- guess.js
+++ guess.js
@@ -1,21 +1,36 @@
 var prompt = require('prompt');
 
 var answer = Math.floor( Math.random() * 100 ) + 1;
+var attempts = 4;
 console.log("Answer: " + answer);
 
-console.log("Guess a number from 0 to 100:");
 
 prompt.start();
-prompt.get( ['guess'], function (err, result) {
-    if (err) return err;
+console.log("Guess a number from 0 to 100:");
 
-    var guess = Number(result.guess);
-    
-    if (guess === answer) {
-        console.log("You win!");
-    } else if (guess < answer) {
-        console.log("Your guess is too low.")
-    } else if (guess > answer) {
-        console.log("Your guess is too high!")
-    }
-})
+getGuess();
+
+//recursively gets user input
+function getGuess () {
+    prompt.get( ['guess'], function (err, result) {
+        if (err) return err;
+        attempts--;
+        
+        var guess = Number(result.guess);
+        
+        if (guess === answer) {
+            console.log("You win!");
+        } 
+        //if guess is wrong but user still has attempts...
+        else if (attempts > 0) {
+            var hint = guess > answer ? 'too high' : 'too low';
+            
+            console.log("Your guess is " + hint + ". You have " + attempts + " attempts left. Try again.");
+            
+            getGuess();
+        } 
+        else {
+            console.log("You lose! The answer was " + answer + ". Better luck next time.");
+        }
+    });
+}
