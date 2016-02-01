127723b2189220a8cc70c402b4fce9bb960997f4
Phil Rajchgot <tophil@outlook.com>
2016-02-01 18:41:49 +0000

    Rounding values to 2 digits

--- iss.js
+++ iss.js
@@ -3,7 +3,9 @@ var request = require("request");
 request("http://api.open-notify.org/iss-now.json", function (err, response, body) {
     if (!err && response.statusCode === 200) {
         var data = JSON.parse(body);
+        var lat = Number( data.iss_position.latitude.toFixed(2) );
+        var long = Number( data.iss_position.longitude.toFixed(2) );
         
-        console.log("The position of the ISS is...\nLatitude: " + data.iss_position.latitude + "\nLongitude: " + data.iss_position.longitude);    
+        console.log("The position of the ISS is...\nLatitude: " + lat + "\nLongitude: " + long);    
     }
 });
