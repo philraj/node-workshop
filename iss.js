4d033ee8d81795e15e97863c133ada858b194fcf
Phil Rajchgot <tophil@outlook.com>
2016-02-01 18:29:21 +0000

    Making output more readable

--- iss.js
+++ iss.js
@@ -3,6 +3,7 @@ var request = require("request");
 request("http://api.open-notify.org/iss-now.json", function (err, response, body) {
     if (!err && response.statusCode === 200) {
         var data = JSON.parse(body);
-        console.log(JSON.stringify(data));        
+        
+        console.log("The position of the ISS is...\nLatitude: " + data.iss_position.latitude + "\nLongitude: " + data.iss_position.longitude);    
     }
 });
