0ddc51e14844985b04507f29363e5f1d68a5bbcb
Phil Rajchgot <tophil@outlook.com>
2016-02-02 03:40:30 +0000

    implemented asking the user if they want to restart, once they win or lose. resets attempts to 4 and goes deeper into the recursive calls (I hope we don't overflow the stack)

--- guess.js
+++ guess.js
@@ -1,14 +1,19 @@
 var prompt = require('prompt');
-
-var answer = Math.floor( Math.random() * 100 ) + 1;
+var answer = 0;
 var attempts = 4;
-console.log("Answer: " + answer);
 
-
+startGame();
 prompt.start();
-console.log("Guess a number from 0 to 100:");
-
 getGuess();
+
+
+
+
+function startGame () {
+    answer = Math.floor( Math.random() * 100 ) + 1;
+    console.log("Answer: " + answer);
+    console.log("Guess a number from 0 to 100:"); 
+}
 
 //recursively gets user input
 function getGuess () {
@@ -20,17 +25,32 @@ function getGuess () {
         
         if (guess === answer) {
             console.log("You win!");
+            askToRestart();
         } 
         //if guess is wrong but user still has attempts...
         else if (attempts > 0) {
             var hint = guess > answer ? 'too high' : 'too low';
             
             console.log("Your guess is " + hint + ". You have " + attempts + " attempts left. Try again.");
-            
             getGuess();
         } 
         else {
-            console.log("You lose! The answer was " + answer + ". Better luck next time.");
+            console.log("You lose! The answer was " + answer + ".");
+            askToRestart();
+        }
+    });
+}
+
+function askToRestart () {
+    console.log("Play again? Enter 'y' to start over.");
+    
+    prompt.get(['input'], function (err, result) {
+        if (err) return err;
+        
+        if (result.input === "y" || result.input === "Y") {
+            attempts = 4;
+            startGame();
+            getGuess();
         }
     });
 }
